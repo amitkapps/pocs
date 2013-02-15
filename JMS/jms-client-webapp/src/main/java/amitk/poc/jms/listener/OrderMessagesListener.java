b1d00a66050e8852c09cada7d479a1a1a7ab5de3
Amit Kapoor <amitk.techno@gmail.com>
2013-02-15 11:42:25 -0700

    JMS

--- JMS/jms-client-webapp/src/main/java/amitk/poc/jms/listener/OrderMessagesListener.java
+++ JMS/jms-client-webapp/src/main/java/amitk/poc/jms/listener/OrderMessagesListener.java
@@ -1,27 +1,51 @@
-package amitk.poc.jms;
+package amitk.poc.jms.listener;
 
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jms.*;
-import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 import java.util.Properties;
 
 /**
- * Unit test for simple App.
+ * Created with IntelliJ IDEA.
+ * User: axk
+ * Date: 9/23/12
+ * Time: 1:16 AM
+ * To change this template use File | Settings | File Templates.
  */
-public class JMSReceiverListenerTest {
-
-    private static Logger logger = LoggerFactory.getLogger(JMSReceiverListenerTest.class);
-
-    @Test
-    public void testReceiveMessage() {
-        logger.info("Test");
+public class OrderMessagesListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        //To change body of implemented methods use File | Settings | File Templates.
+        logger.info("Setting up Order Message Listener");
+        try {
+            startOrderMessageListener();
+        } catch (Exception e) {
+            logger.error("Error starting Order Message Listener", e);
+        }
     }
 
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        //To change body of implemented methods use File | Settings | File Templates.
+        try {
+            conn.stop();
+            session.close();
+            conn.close();
+        } catch (JMSException e) {
+            logger.error("Error stopping message listener", e);
+        }
+
+    }
+
+
+
+
+    private static Logger logger = LoggerFactory.getLogger(OrderMessagesListener.class);
 
     QueueConnection conn;
     QueueSession session;
@@ -34,69 +58,46 @@ public class JMSReceiverListenerTest {
         public void onMessage(Message msg) {
             TextMessage tm = (TextMessage) msg;
             try {
-                logger.info("onMessage, recv text=" + tm.getText());
+                logger.info("Received text message=" + tm.getText());
             } catch (Throwable t) {
                 t.printStackTrace();
             }
         }
     }
 
-    public void setupPTP()
+    private void setupQueueConnection()
             throws JMSException,
             NamingException {
 
         Properties p = new Properties();
+/*
         p.put(Context.INITIAL_CONTEXT_FACTORY,
                 "org.jnp.interfaces.NamingContextFactory");
         p.put(Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces");
         p.put(Context.PROVIDER_URL, "jnp://10.8.7.142:1099/");
+*/
 
 
         InitialContext iniCtx = new InitialContext(p);
-        Object tmp = iniCtx.lookup("jms/cf/chassis");
+        Object tmp = iniCtx.lookup("jms/cf/ordersystem");
         QueueConnectionFactory qcf = (QueueConnectionFactory) tmp;
         conn = qcf.createQueueConnection();
-        que = (Queue) iniCtx.lookup("jms/queue/chassis");
+        que = (Queue) iniCtx.lookup("jms/queue/orders");
         session = conn.createQueueSession(false,
                 QueueSession.AUTO_ACKNOWLEDGE);
         conn.start();
     }
 
-    public void sendRecvAsync(String text)
+    private void startOrderMessageListener()
             throws JMSException,
             NamingException {
-        logger.info("Begin sendMessage");
+        logger.info("Begin receive messages");
         // Setup the PTP connection, session
-        setupPTP();
+        setupQueueConnection();
 
         // Set the async listener
         QueueReceiver recv = session.createReceiver(que);
         recv.setMessageListener(new ExListener());
-
-        // Send a text msg
-        QueueSender send = session.createSender(que);
-        TextMessage tm = session.createTextMessage(text);
-        send.send(tm);
-        logger.info("sendMessage, sent text=" + tm.getText());
-        send.close();
-        logger.info("End sendMessage");
-    }
-
-    public void stop()
-            throws JMSException {
-        conn.stop();
-        session.close();
-        conn.close();
-    }
-
-    public static void main(String args[])
-            throws Exception {
-        logger.info("Begin SendRecvClient, now=" + System.currentTimeMillis());
-        JMSReceiverListenerTest client = new JMSReceiverListenerTest();
-        client.sendRecvAsync("A text msg");
-        client.stop();
-        logger.info("End SendRecvClient");
-        System.exit(0);
     }
 
 
--- JMS/jms-client-webapp/src/main/java/amitk/poc/jms/producer/JMSMessageSender.java
+++ JMS/jms-client-webapp/src/main/java/amitk/poc/jms/producer/JMSMessageSender.java
@@ -1,6 +1,5 @@
-package amitk.poc.jms;
+package amitk.poc.jms.producer;
 
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,46 +12,28 @@ import java.util.Properties;
 /**
  * Unit test for simple App.
  */
-public class JMSMessageSenderTest {
+public class JMSMessageSender {
 
-    private static Logger logger = LoggerFactory.getLogger(JMSMessageSenderTest.class);
-
-    @Test
-    public void testReceiveMessage() {
-        logger.info("Test");
-    }
-
+    private static Logger logger = LoggerFactory.getLogger(JMSMessageSender.class);
 
     QueueConnection conn;
     QueueSession session;
     Queue que;
 
-    public static class ExListener
-            implements MessageListener {
-        private static Logger logger = LoggerFactory.getLogger(ExListener.class);
-
-        public void onMessage(Message msg) {
-            TextMessage tm = (TextMessage) msg;
-            try {
-                logger.info("onMessage, recv text=" + tm.getText());
-            } catch (Throwable t) {
-                t.printStackTrace();
-            }
-        }
-    }
-
     public void setupPTP()
             throws JMSException,
             NamingException {
 
+/*
         Properties p = new Properties();
         //p.put(Context.INITIAL_CONTEXT_FACTORY, "weblogic.jndi.WLInitialContextFactory");
         p.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
         p.put(Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces");
         //p.put(Context.PROVIDER_URL, "t3://10.201.2.59:8103");
         p.put(Context.PROVIDER_URL, "jnp://10.8.7.121:1099/");
+*/
 
-        InitialContext iniCtx = new InitialContext(p);
+        InitialContext iniCtx = new InitialContext();
         Object tmp = iniCtx.lookup("jms.cf.eir.wl");
         logger.info("Obtained EirMQConnectionFactory " + tmp);
         QueueConnectionFactory qcf = (QueueConnectionFactory) tmp;
@@ -81,6 +62,7 @@ public class JMSMessageSenderTest {
         logger.info("sendMessage, sent text=" + tm.getText());
         send.close();
         logger.info("End sendMessage");
+        stop();
     }
 
     public void stop()
@@ -88,16 +70,6 @@ public class JMSMessageSenderTest {
         conn.stop();
         session.close();
         conn.close();
-    }
-
-    public static void main(String args[])
-            throws Exception {
-        logger.info("Begin SendRecvClient, now=" + System.currentTimeMillis());
-        JMSMessageSenderTest client = new JMSMessageSenderTest();
-        client.sendMessage("MATU457203:70004  :376232    :201210290438");
-        client.stop();
-        logger.info("End SendRecvClient");
-        System.exit(0);
     }
 
 
