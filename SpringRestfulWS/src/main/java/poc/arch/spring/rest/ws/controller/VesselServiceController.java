67cf2d7929e10848f08154253e9a6b1529c96664
Amit Kapoor <amitk.techno@gmail.com>
2014-06-19 16:29:11 -0700

    With Spring security for rest w/s

--- SpringRestfulWS/src/main/java/poc/arch/spring/rest/ws/controller/VesselServiceController.java
+++ SpringRestfulWS/src/main/java/poc/arch/spring/rest/ws/controller/VesselServiceController.java
@@ -4,27 +4,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 import poc.arch.spring.rest.ws.domain.Vessel;
 
-import java.util.List;
-
 @Controller
-@RequestMapping("/vesselservice")
+@RequestMapping("/vesselService")
 public class VesselServiceController {
 
     private static Logger log = LoggerFactory.getLogger(VesselServiceController.class);
 
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, value = "/vesselList")
     @ResponseStatus(HttpStatus.OK)
-    public @ResponseBody Vessel getVessel() {
-        Vessel vessel = new Vessel();
-        vessel.setVesselCd("MAT");
-        vessel.setVesselName("MATSONIA");
-        return vessel;
+    public @ResponseBody Vessel[] getVesselList(@RequestParam(value="terminalId", required = true) int terminalId) {
+        log.info("Request vessel list for terminalId={}", terminalId);
+        Vessel vessel1 = new Vessel();
+        vessel1.setVesselCd("MAT");
+        vessel1.setVesselName("MATSONIA");
+
+        Vessel vessel2 = new Vessel();
+        vessel2.setVesselCd("RJP");
+        vessel2.setVesselName("R J PFEIFER");
+        return new Vessel[]{vessel1, vessel2};
     }
 }
---
+++ SpringRestfulWS/src/main/java/poc/arch/spring/rest/ws/security/VINsightSpringSecurityAuthenticationProvider.java
@@ -0,0 +1,35 @@
+package poc.arch.spring.rest.ws.security;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Authentication provider that matches the credentials against the VINsight use database.
+ */
+public class VINsightSpringSecurityAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String name = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        System.out.println("Authenticating user: " + name);
+        if (name.equals("admin") && password.equals("system")) {
+            List<GrantedAuthority> grantedAuths = new ArrayList<GrantedAuthority>();
+            grantedAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
+            Authentication auth = new UsernamePasswordAuthenticationToken(name, password, grantedAuths);
+            return auth;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }}
