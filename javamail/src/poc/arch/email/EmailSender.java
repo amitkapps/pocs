36bf897d78ce95c54448997e04efa5e4fec9d76b
Amit Kapoor <amitk.techno@gmail.com>
2014-07-29 21:21:29 -0700

    mail with attachments

--- javamail/src/poc/arch/email/EmailSender.java
+++ javamail/src/poc/arch/email/EmailSender.java
@@ -1,7 +1,12 @@
 package poc.arch.email;
 
 import java.util.*;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.*;
+import javax.mail.event.TransportEvent;
+import javax.mail.event.TransportListener;
 import javax.mail.internet.*;
 
 /**
@@ -15,14 +20,14 @@ public class EmailSender {
 
     // File Name SendEmail.java
 
-        public static void main(String [] args){
-            sendEmails(100, 100);
-        }
+    public static void main(String [] args){
+        sendEmails(1, 100, true, "110_20140715103155_0_notify.pdf");
+    }
 
-    private static void sendEmails(int totalEmails, long delay) {
-        System.out.println("Send " + totalEmails + " mails with " + delay + " millis delay each");
+    private static void sendEmails(int totalEmails, long delay, boolean addAttachment, String fileName) {
+        System.out.println("Send " + totalEmails + " mails with " + delay + " millis delay each via host");
         for(int i=1; i<=totalEmails; i++){
-            sendEmail(i);
+            sendEmail(i, addAttachment, fileName);
             delay(delay);
         }
     }
@@ -37,12 +42,16 @@ public class EmailSender {
         }
     }
 
-    private static void sendEmail(int counter) {
+    private static void sendEmail(int counter, boolean addAttachment, String fileName) {
         // Recipient's email ID needs to be mentioned.
 //        String to = "amit.kapps@gmail.com";
-        String to = "matson.ses@gmail.com";
+//        String to = "akapoor@matson.com";
         //Test amazon ses email id to simulate bounce/out of the office/complaints(spam) etc.
+//        String to = "akapoor@matson.com";
+        //Amazon ses simulator - http://docs.aws.amazon.com/ses/latest/DeveloperGuide/mailbox-simulator.html
+//        String to = "matson.ses@gmail.com";
 //        String to = "bounce@simulator.amazonses.com";
+        String to = "nobody234@matson.com";
 //        String to = "ooto@simulator.amazonses.com";
 //        String to = "complaint@simulator.amazonses.com";
 //        String to = "suppressionlist@simulator.amazonses.com"; //emails that are on ses suppressionlist for previously known hard bounces
@@ -50,12 +59,17 @@ public class EmailSender {
         // Sender's email ID needs to be mentioned
         String from = "akapoor@matson.com";
 //        String from = "matson.ses@gmail.com";
+        // String from = "notify@matson.com";
+//        String from = "1webteamatphx@matson.com";
 
         // Assuming you are sending email from localhost
         //Local Centos
 //        String host = "10.8.5.35"; //Local Centos
 //        String port = "1025"; //Local Centos
         String host = "10.8.7.60"; // Dev apache
+//        String host = "10.201.2.80"; // pre apache1
+//        String host = "128.167.98.113"; //PRE LB
+//        String host = "171.73.201.44"; //PROD LB
         String port = "25"; //Dev apache
 
         // Get system properties
@@ -75,20 +89,69 @@ public class EmailSender {
 
             // Set From: header field of the header.
             message.setFrom(new InternetAddress(from));
+            message.setReplyTo(new InternetAddress[]{ new InternetAddress("matson.ses@mgmail.com")});
 
             // Set To: header field of the header.
             message.addRecipient(Message.RecipientType.TO,
                     new InternetAddress(to));
 
             // Set Subject: header field
-            message.setSubject("Amazon ses mail from remote java! count="+counter);
+//            String subject = "Amazon ses mail from remote java! count=" + counter;
+            String subject = "This is a test email messages sent via amazon ses";
+            message.setSubject(subject);
 
             // Now set the actual message
-            message.setText("remote java program > linux sendmail > amazon ses"+counter);
+//            String bodyText = "remote java program > linux sendmail > amazon ses" + counter;
+            String bodyText = "Test email sent via amazon which will bounce back and send us the original email with attachment via ses mailer daemon";
+            message.setText(bodyText);
+
+
+            if(addAttachment){
+                BodyPart messageBodyPart = new MimeBodyPart();
+
+                messageBodyPart.setText(bodyText + "\n\n");
+
+                Multipart multipart = new MimeMultipart();
+
+                multipart.addBodyPart(messageBodyPart);
+
+                BodyPart messageFileAttachment = new MimeBodyPart();
+
+                DataSource source = new FileDataSource(fileName);
+
+                messageFileAttachment.setDataHandler(new DataHandler(source));
+
+                messageFileAttachment.setFileName(fileName);
+
+                multipart.addBodyPart(messageFileAttachment);
+
+                message.setContent(multipart);
+            }
+
+
+
+
+            Transport transport = session.getTransport("smtp");
+            transport.addTransportListener(new TransportListener(){
+                @Override
+                public void messageDelivered(TransportEvent transportEvent) {
+                    System.out.println("Message Delivered" + transportEvent);
+                }
+
+                @Override
+                public void messageNotDelivered(TransportEvent transportEvent) {
+                    System.out.println("Message Not Delivered" + transportEvent);
+                }
+
+                @Override
+                public void messagePartiallyDelivered(TransportEvent transportEvent) {
+                    System.out.println("Message Partially Delivered" + transportEvent);
+                }
+            });
 
             // Send message
-            Transport.send(message);
-            System.out.println("Sent message successfully to "+ to + " through mailhost "+ host +":"+port+", counter="+counter);
+            transport.send(message);
+            System.out.println("Sent message successfully from " + from+ ", to "+ to + " through mailhost "+ host +":"+port+", counter="+counter + ", with attachement [" + fileName + "] ?" + addAttachment);
         }catch (MessagingException mex) {
             mex.printStackTrace();
         }
